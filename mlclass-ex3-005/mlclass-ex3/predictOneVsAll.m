67daa7409fe0831dc484d3559052ba34033509c8
jackieqi@gmail.com <jackieqi@gmail.com>
2014-04-05 23:47:54 -0700

    vectorized predicOneVsAll

--- mlclass-ex3-005/mlclass-ex3/predictOneVsAll.m
+++ mlclass-ex3-005/mlclass-ex3/predictOneVsAll.m
@@ -31,15 +31,7 @@ X = [ones(m, 1) X];
 %       
 
 initial_p = X * all_theta';
-max_p = max(initial_p, [], 2);
-
-% find index of max_p in initial_p in each row
-for i = 1: m
-	p(i) = find(initial_p(i, :) == max_p(i));
-endfor
-
-
-
+[max_p, p] = max(initial_p, [], 2);
 
 
 % =========================================================================
